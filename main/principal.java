8bb8df806f4ea5829e7380a8ddc16b2320245d66
cepillin <yonathan41204.com@gmail.com>
2023-11-28 07:52:36 -0600

    Agruegue metodo main para poder wachar la ventana del Basta

--- main/principal.java
+++ main/principal.java
@@ -1,7 +1,10 @@
 package main;
 
+import vista.panelBasta;
+
 public class principal {
     public static void main(String[] args) {
-        
+        panelBasta basta = new panelBasta();
+        basta.setVisible(true);
     }
 }
